e6d009ffc3ba954b3e6c09160ca6c50150e05ca5
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-03-12 02:22:04 +0100

    Actually solve pot

--- week-1/pot/pot.cpp
+++ week-1/pot/pot.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 
-using namespace std;
+int pow(int d, int exp) {
+  int ret = d;
+  for (int i = 0; i < exp - 1; i++) {
+    ret *= d;
+  }
+  return ret;
+}
 
 int main() {
-	long long a;
-	int b;
-	scanf("%lld %d", &a, &b);
+  int a, b;
+  scanf("%d %d", &a, &b);
 
-	long long ans = 1;
-	while (b > 0) {
-		if (b & 1) {
-			ans *= a;
-		}
-		a *= a;
-		b >>= 1;
-	}
+  int d = a % 10;
+  if (d == 0) {
+    printf("0\n");
+    return 0;
+  }
 
-	cout << ans % 10 << endl;
-	return 0;
+  int exp = b % 4;
+  if (exp == 0) {
+    exp = 4;
+  }
+
+  printf("%d\n", pow(d, exp) % 10);
+
+  return 0;
 }
