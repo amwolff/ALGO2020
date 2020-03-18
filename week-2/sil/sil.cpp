4f4bb1a12d433a601bbc336953685972b13853cf
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-03-18 21:55:46 +0100

    Add sil

---
+++ week-2/sil/sil.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+
+  switch (n) {
+    case 0:
+      printf("%d\n", 1);
+      return 0;
+    case 1:
+      printf("%d\n", 1);
+      return 0;
+    case 2:
+      printf("%d\n", 2);
+      return 0;
+    case 3:
+      printf("%d\n", 6);
+      return 0;
+    case 4:
+      printf("%d\n", 4);
+      return 0;
+  }
+  printf("%d\n", 0);
+  return 0;
+}
