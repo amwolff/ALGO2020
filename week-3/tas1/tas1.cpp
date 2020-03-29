c396c1e3c9918fc7107af3012d4aebad0e4bcea6
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-03-30 00:07:17 +0200

    Add tas1

---
+++ week-3/tas1/tas1.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n, t;
+  scanf("%d %d\n", &n, &t);
+
+  vector<int> a(1 << n);
+  for (size_t i = 0; i < a.size(); i++) {
+    scanf("%d\n", &a[i]);
+  }
+
+  if (t % 2 == 1) {
+    reverse(a.begin(), a.end());
+  }
+
+  for (size_t i = 0, s = a.size(); i < s; i++) {
+    if (i == s - 1) {
+      printf("%d\n", a[i]);
+      return 0;
+    }
+    printf("%d ", a[i]);
+  }
+
+  return 0;
+}
