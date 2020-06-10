35c7b3abc0c30bd18725ac01a2854819077d7534
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-06-10 23:11:49 +0200

    Optimize wks a little

--- week-12/wks/wks.cpp
+++ week-12/wks/wks.cpp
@@ -14,16 +14,18 @@ int main() {
   }
 
   vector m(n, 1);
+  int maximum = 1;
   for (size_t i = 0; i < v.size(); i++) {
     for (size_t j = 0; j < i; j++) {
       const int t = m[j] + 1;
       if (v[i] > v[j] && m[i] < t) {
         m[i] = t;
+        maximum = max(maximum, t);
       }
     }
   }
 
-  printf("%d\n", *max_element(m.cbegin(), m.cend()));
+  printf("%d\n", maximum);
 
   return 0;
 }
