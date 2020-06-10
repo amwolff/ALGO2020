9081adfbc7ea8d882bf5117fcfb8f7ddc38d7f4f
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-06-10 22:50:51 +0200

    Add wks (60)

---
+++ week-12/wks/wks.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  scanf("%d\n", &n);
+
+  vector<int> v(n);
+  for (size_t i = 0; i < v.size(); i++) {
+    scanf("%d\n", &v[i]);
+  }
+
+  vector m(n, 1);
+  for (size_t i = 0; i < v.size(); i++) {
+    for (size_t j = 0; j < i; j++) {
+      const int t = m[j] + 1;
+      if (v[i] > v[j] && m[i] < t) {
+        m[i] = t;
+      }
+    }
+  }
+
+  printf("%d\n", *max_element(m.cbegin(), m.cend()));
+
+  return 0;
+}
